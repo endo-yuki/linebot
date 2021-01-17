2851b182dd2078fb685732cf6b41de078e4ed4dd
endo-yuki <engineer@endo-yuki.net>
2021-01-17 13:34:46 +0900

    Upload image to dropbox on LINEBot

--- cmd/echo_bot/main.go
+++ cmd/echo_bot/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox"
+	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/files"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"gopkg.in/ini.v1"
 )
@@ -13,15 +15,17 @@ import (
 type Config struct {
 	channelSecrt string
 	channelToken string
+	dropboxToken string
 }
 
-var Cnf Config
+var conf Config
 
 func init() {
 	c, _ := ini.Load("config.ini")
-	Cnf = Config{
+	conf = Config{
 		channelSecrt: c.Section("lineBotApi").Key("secret").String(),
 		channelToken: c.Section("lineBotApi").Key("token").String(),
+		dropboxToken: c.Section("DropboxApi").Key("token").String(),
 	}
 }
 
@@ -32,8 +36,8 @@ func main() {
 
 func lineHandler(w http.ResponseWriter, r *http.Request) {
 	bot, err := linebot.New(
-		Cnf.channelSecrt,
-		Cnf.channelToken,
+		conf.channelSecrt,
+		conf.channelToken,
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -50,14 +54,39 @@ func lineHandler(w http.ResponseWriter, r *http.Request) {
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
-			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+			case *linebot.ImageMessage:
+				content, err := bot.GetMessageContent(message.ID).Do()
+				if err != nil {
+					log.Println(err)
+				}
+				filename := message.ID + ".png"
+				file, err := os.Create(filename)
+				if err != nil {
+					log.Println(err)
+				}
+
+				if _, err = io.Copy(file, content.Content); err != nil {
+					log.Println(err)
+				}
+
+				config := dropbox.Config{
+					Token: conf.dropboxToken,
+				}
+				cli := files.New(config)
+
+				req := files.NewCommitInfo("/" + filename)
+				f, _ := os.Open(filename)
+				_, err = cli.Upload(req, f)
+				if err != nil {
+					log.Print(err)
+					return
+				}
+
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("アップロードが完了しました")).Do(); err != nil {
 					log.Print(err)
 				}
-			case *linebot.StickerMessage:
-				replyMessage := fmt.Sprintf(
-					"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+			default:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("画像を送信してください")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
